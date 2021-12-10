fccaedf036edc29564cc08bd1b9ca75e8377542c
pranta <ppalit.cghs@gmail.com>
2021-12-10 19:06:01 +0600

    check hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(BusinessCard());
+}
+
+class BusinessCard extends StatelessWidget {
+  const BusinessCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       // set it to false to hide debug banner in corner
       debugShowCheckedModeBanner: false,
 
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
