3e1c64e1e6fb5b392932a4e3773d76fa2f841c7a
pranta <ppalit.cghs@gmail.com>
2021-12-10 18:53:41 +0600

    business card using flutter

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      // set it to false to hide debug banner in corner
+      debugShowCheckedModeBanner: false,
+
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+    ),
+  );
+}
